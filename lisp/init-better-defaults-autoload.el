22a9d3c9b32efa750debcffe54b98e703583473e
lilkr <maintiendrai@gmail.com>
2017-08-26 15:12:33 +0800

    add indent-buffer

---
+++ lisp/init-better-defaults-autoload.el
@@ -0,0 +1,24 @@
+;;; init-better-defaults-autoload.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "init-better-defaults" "init-better-defaults.el"
+;;;;;;  (22944 17629 0 0))
+;;; Generated autoloads from init-better-defaults.el
+
+(autoload 'test-autoload "init-better-defaults" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+(provide 'init-better-defaults-autoload)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; init-better-defaults-autoload.el ends here
--- lisp/init-better-defaults.el
+++ lisp/init-better-defaults.el
@@ -5,14 +5,14 @@
 (global-linum-mode 1)
 
 
- (setq-default abbrev-mode t)
+(setq-default abbrev-mode t)
 
-  (define-abbrev-table 'global-abbrev-table '(
-                                              ;; signature
-                                              ("8zl" "zilongshanren")
-                                              ;; emacs regex
-                                              
-                                              ))
+(define-abbrev-table 'global-abbrev-table '(
+					    ;; signature
+					    ("8zl" "zilongshanren")
+					    ;; emacs regex
+					    
+					    ))
 
 (setq make-backup-files nil)
 
@@ -25,6 +25,25 @@
 (add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
 
 (delete-selection-mode t)
+
+
+(defun indent-buffer ()
+  "Indent the currently visited buffer."
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(defun indent-region-or-buffer ()
+  "Indent a region if selected, otherwise the whole buffer."
+  (interactive)
+  (save-excursion
+    (if (region-active-p)
+	(progn
+	  (indent-region (region-beginning) (region-end))
+	  (message "Indented selected region."))
+      (progn
+	(indent-buffer)
+	(message "Indented buffer.")))))
+
 
 ;;;###autoload
 (defun test-autoload()
