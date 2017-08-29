f7fa0ae4070f9c663dbe3633e6d28196449cc5e3
lilkr <maintiendrai@gmail.com>
2017-08-30 00:12:53 +0800

    add js2-refactor

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -19,6 +19,7 @@
 				   popwin
 				   reveal-in-osx-finder
 				   web-mode
+				   js2-refactor
 
                 )  "Default packages")
 
@@ -60,10 +61,40 @@
 ;; config js2-mode for js files
 (setq auto-mode-alist
       (append
-       '(("\\.js\\'" . js2-mode))
+       '(("\\.js\\'" . js2-mode)
+	 ("\\.html\\'" . web-mode))
        auto-mode-alist))
 
 (global-company-mode t)
+
+
+;; config for web mode
+ (defun my-web-mode-indent-setup ()
+    (setq web-mode-markup-indent-offset 2) ; web-mode, html tag in html file
+    (setq web-mode-css-indent-offset 2)    ; web-mode, css in html file
+    (setq web-mode-code-indent-offset 2)   ; web-mode, js code in html file
+    )
+
+(add-hook 'web-mode-hook 'my-web-mode-indent-setup)
+
+ (defun my-toggle-web-indent ()
+    (interactive)
+    ;; web development
+    (if (or (eq major-mode 'js-mode) (eq major-mode 'js2-mode))
+        (progn
+          (setq js-indent-level (if (= js-indent-level 2) 4 2))
+          (setq js2-basic-offset (if (= js2-basic-offset 2) 4 2))))
+
+    (if (eq major-mode 'web-mode)
+        (progn (setq web-mode-markup-indent-offset (if (= web-mode-markup-indent-offset 2) 4 2))
+               (setq web-mode-css-indent-offset (if (= web-mode-css-indent-offset 2) 4 2))
+               (setq web-mode-code-indent-offset (if (= web-mode-code-indent-offset 2) 4 2))))
+    (if (eq major-mode 'css-mode)
+        (setq css-indent-offset (if (= css-indent-offset 2) 4 2)))
+
+    (setq indent-tabs-mode nil))
+
+  (global-set-key (kbd "C-c t i") 'my-toggle-web-indent)
 
 (load-theme 'monokai t) 
 
