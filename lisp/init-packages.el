decf7a9e9d3c340db1071ea66b86bac874622d7c
lilkr <maintiendrai@gmail.com>
2017-09-01 08:11:51 +0800

    add iedit expand-region

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -20,6 +20,8 @@
 				   reveal-in-osx-finder
 				   web-mode
 				   js2-refactor
+				   expand-region
+				   iedit
 
                 )  "Default packages")
 
@@ -102,10 +104,31 @@
 
 (global-set-key (kbd "M-s i") 'counsel-imenu)
 
+ (defun js2-imenu-make-index ()
+        (interactive)
+        (save-excursion
+          ;; (setq imenu-generic-expression '((nil "describe\\(\"\\(.+\\)\"" 1)))
+          (imenu--generic-function '(("describe" "\\s-*describe\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+                                     ("it" "\\s-*it\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+                                     ("test" "\\s-*test\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+                                     ("before" "\\s-*before\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+                                     ("after" "\\s-*after\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+                                     ("Function" "function[ \t]+\\([a-zA-Z0-9_$.]+\\)[ \t]*(" 1)
+                                     ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*=[ \t]*function[ \t]*(" 1)
+                                     ("Function" "^var[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*=[ \t]*function[ \t]*(" 1)
+                                     ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*()[ \t]*{" 1)
+                                     ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*:[ \t]*function[ \t]*(" 1)
+                                     ("Task" "[. \t]task([ \t]*['\"]\\([^'\"]+\\)" 1)))))
+  (add-hook 'js2-mode-hook
+                (lambda ()
+                  (setq imenu-create-index-function 'js2-imenu-make-index)))
+
 (load-theme 'monokai t) 
 
 (require 'popwin)
    ;;when require, wh(setq company-minimum-prefix-length 1)en not require
 (popwin-mode t)
+
+(global-set-key (kbd "C-=") 'er/expand-region)
 
 (provide 'init-packages)
